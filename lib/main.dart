b7c51a6a971eed26da76014757ed46438c40f598
Jose Manuel <manuelvaldez8642@gmail.com>
2020-02-13 15:56:30 -0600

    interface update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'runProyect.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pastes Simulator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Paste Simulator'),
     );
   }
 }
@@ -26,57 +28,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Scaffold build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Card(
+                elevation: 12,
+                margin: EdgeInsets.all(25),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  child: Text('data'),
+                )
+              ),
+              Text('data',style: TextStyle(
+                fontSize: 35,
+                shadows:  <Shadow>[
+                  Shadow(
+                    offset: Offset(3.0, 3.0),
+                    blurRadius: 25,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ],
+              ),)
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Card(
+                  elevation: 12,
+                  margin: EdgeInsets.all(25),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: Text('data'),
+                  )
+                ),
+                Card(
+                  elevation: 12,
+                  margin: EdgeInsets.all(25),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: Text('data'),
+                  )
+                ),
+            ],
+          ),
+          Card(
+            elevation: 12,
+            margin: EdgeInsets.all(25),
+            child: Container(
+              width: 1000,
+              height: 100,
+              child: Text('data'),
+            )
+          ),
+          
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        // onPressed: ,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
